fad2432c33bd43c83e68c7490cffd7682dce8b6e
Anders Olme <anders.olme@gmail.com>
2011-09-25 11:48:27 +0200

    replace_a_b_c now working :)!"

--- all_as.pl
+++ all_as.pl
@@ -1,8 +1,13 @@
 all_as([]).
 all_as([a|Tail]):-all_as(Tail).
 
-%replace_a_b_c([],[]).
-%replace_a_b_c([a|Tail],[b|NTail]) :-
+replace_a_b_c([],[]).
+replace_a_b_c([a|Tail],[b|NTail]) :- replace_a_b_c(Tail, NTail).
+replace_a_b_c([b|Tail],[c|NTail]) :- replace_a_b_c(Tail, NTail).
+replace_a_b_c([c|Tail],[a|NTail]) :- replace_a_b_c(Tail, NTail).
+%% Fourth case: the first element is something else.
+replace_a_b_c([X|Tail],[X|NTail]) :- X \= a, X \= b, X \= c,
+              replace_a_b_c(Tail,NTail).
 
 concatenate([],L,L).
 concatenate([Head|Tail],L,[Head|NewTail]) :-
